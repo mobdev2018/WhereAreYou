2e9b60892fc3e04480e1a41f038212eed2f04e60
Arkadiy <mobdev2018@gmail.com>
2018-07-23 00:16:53 -0700

    added Login UI;

---
+++ app/src/main/java/com/av/whereareyou/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.av.whereareyou;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
