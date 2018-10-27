9da1e1647592d1ad83cbcae57f93d890716c03a3
superdev <super.dev0714@gmail.com>
2018-10-28 00:15:11 +0200

    added gps tracking feature.

--- app/src/main/java/com/av/whereareyou/IntroActivity.java
+++ app/src/main/java/com/av/whereareyou/IntroActivity.java
@@ -1,22 +1,52 @@
 package com.av.whereareyou;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class IntroActivity extends Activity {
 
+
+    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    private static final int REQUEST_CHECK_SETTINGS = 10001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         ButterKnife.bind(this);
+
+        displayLocationSettingsRequest(this);
+        checkAndRequestPermissions();
+
+        Intent intent = new Intent(getApplicationContext(), TrackLocationService.class);
+        getApplication().startService(intent);
     }
 
     @OnClick(R.id.btn_continue)
@@ -28,6 +58,73 @@ public class IntroActivity extends Activity {
     @Override
     public void onBackPressed() {
 
+    }
+
+    private  boolean checkAndRequestPermissions() {
+
+        int writePermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_SETTINGS);
+        int permissionLocation = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        int permissionCoarseLocation = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        List<String> listPermissionsNeeded = new ArrayList<>(4);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.WRITE_SETTINGS);
+        }
+        if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (permissionCoarseLocation != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+
+    private void displayLocationSettingsRequest(Context context) {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Log.i("MyDeviceLocation", "All location settings are satisfied.");
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.i("MyDeviceLocation", "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the result
+                            // in onActivityResult().
+                            status.startResolutionForResult(IntroActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.i("MyDeviceLocation", "PendingIntent unable to execute request.");
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i("MyDeviceLocation", "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+                        break;
+                }
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/av/whereareyou/TrackLocationService.java
@@ -0,0 +1,219 @@
+package com.av.whereareyou;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+
+public class TrackLocationService extends Service {
+
+    private static final String Location_TAG = "MyLocationService";
+
+    private LocationManager mLocationManager = null;
+
+    //FireBase auth object
+    private DatabaseReference mDatabase;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d(Location_TAG, "onCreate");
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        
+        String userId = user.getUid();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        try {
+            database.setPersistenceEnabled(true);
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        }
+
+        mDatabase = database.getReference().child("users").child(userId);
+
+        initializeLocationManager();
+        startLocationTrack();
+    }
+
+    private void startLocationTrack() {
+
+        removeLocationListeners();
+
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 10000, 1,
+                    mLocationListeners[0]);
+
+        } catch (java.lang.SecurityException ex) {
+            Log.i(Location_TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(Location_TAG, "gps provider does not exist " + ex.getMessage());
+        }
+
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 10000, 1,
+                    mLocationListeners[1]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(Location_TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(Location_TAG, "network provider does not exist, " + ex.getMessage());
+        }
+    }
+
+    private void uploadCurrentLocation(Location location) {
+
+        if (location == null || (location.getLongitude() == 0 && location.getLatitude() == 0 )) {
+            return;
+        }
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        mDatabase.child("latitude").setValue(latitude);
+        mDatabase.child("longitude").setValue(longitude);
+
+        Date current = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String strTime = dateFormat.format(current);
+        mDatabase.child("lastTime").setValue(strTime);
+
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        String address = "", city = "", state = "", country = "", postalCode = "", knownName = "";
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+            if (addresses.size() > 0) {
+                address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                city = addresses.get(0).getLocality() == null ? "" : addresses.get(0).getLocality();
+                state = addresses.get(0).getAdminArea() == null ? "" : addresses.get(0).getAdminArea();
+                country = addresses.get(0).getCountryName() == null ? "" : addresses.get(0).getCountryName();
+                postalCode = addresses.get(0).getPostalCode() == null ? "" : addresses.get(0).getPostalCode();
+                knownName = addresses.get(0).getFeatureName() == null ? "" : addresses.get(0).getFeatureName(); // Only if available else return NULL
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
+        mDatabase.child("address").setValue(address);
+        mDatabase.child("city").setValue(city);
+        mDatabase.child("state").setValue(state);
+        mDatabase.child("country").setValue(country);
+        mDatabase.child("postalCode").setValue(postalCode);
+        mDatabase.child("knownName").setValue(knownName);
+    }
+
+    private class LocationListener implements android.location.LocationListener {
+
+        public LocationListener(String provider) {
+            Log.d(Location_TAG, "LocationListener " + provider);
+            Location location = new Location(provider);
+            uploadCurrentLocation(location);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(Location_TAG, "onLocationChanged: " + location);
+            uploadCurrentLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d(Location_TAG, "onProviderDisabled: " + provider);
+            Toast.makeText(getApplicationContext(), "Location is disabled. Please enable your location on Settings.", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d(Location_TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d(Location_TAG, "onStatusChanged: " + provider);
+
+        }
+    }
+
+
+    LocationListener[] mLocationListeners = new LocationListener[] {
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(Location_TAG, "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+
+//        return START_STICKY;
+    }
+
+    private void initializeLocationManager() {
+        Log.d(Location_TAG, "initializeLocationManager");
+
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    private void removeLocationListeners() {
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception ex) {
+                    Log.i(Location_TAG, "fail to remove location listners, ignore", ex);
+                }
+            }
+        }
+    }
+    @Override
+    public void onDestroy()
+    {
+        Log.d(Location_TAG, "onDestroy");
+
+        super.onDestroy();
+    }
+
+}
