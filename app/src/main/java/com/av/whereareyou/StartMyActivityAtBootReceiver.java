1c45485e9733f759d2d4b5caf3e224258d0dd810
mobdev2018 <mobdev2018@gmail.com>
2018-12-29 04:28:17 +0100

    updated.

--- app/src/main/java/com/av/whereareyou/StartMyActivityAtBootReceiver.java
+++ app/src/main/java/com/av/whereareyou/StartMyActivityAtBootReceiver.java
@@ -9,8 +9,8 @@ public class StartMyActivityAtBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent activityIntent = new Intent(context, TrackLocationService.class);
-//            activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            Intent activityIntent = new Intent(context, IntroActivity.class);
+            activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startService(activityIntent);
         }
     }
